7c437ddb61e1081d7333407fcb087f8efac42765
Roger Forner Fabre <hello@rogerforner.com>
2018-06-06 09:49:38 +0200

    template: plantilla HTML que Vue renderitzarà en l'element HTML referenciat

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
 const app = new Vue({
-    el: '#app'
+    // Generar una instància que tingui com a referència l'element HTML amb
+    // identificador app => <div id="app"></div>
+    el: '#app',
+
+    // Afegim una plantilla dintre de l'element HTML referenciat.
+    template: [
+        '<div class="view">',
+            '<game-header></game-header>',                  // Component fill
+            '<game-add @new="addNewGame"></game-add>',      // Component fill amb esdeveniment @new (escoltat pel pare)
+            '<game-list v-bind:games="games"></game-list>', // Component fill amb directiva v-bind (enllaça una propietat interna del component amb un model (games) de l'element pare)
+        '</div>'
+    ].join('')
+        
 });
