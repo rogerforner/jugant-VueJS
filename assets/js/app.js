0172cae02407100e78b2050bea61103946cc62c0
Roger Forner Fabre <hello@rogerforner.com>
2018-06-06 10:15:36 +0200

    Components Pare-Fills + Emissió dades ($emit) de fill a pare + Enviar dades (props) de pare a fill

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,13 @@
+/*
+|-------------------------------------------------------------------------------
+| Instància de Vue
+|-------------------------------------------------------------------------------
+| Generar una instància que tingui com a referència l'element HTML amb
+| identificador app => <div id="app"></div>
+|
+*/
+
 const app = new Vue({
-    // Generar una instància que tingui com a referència l'element HTML amb
-    // identificador app => <div id="app"></div>
     el: '#app',
 
     // Afegim una plantilla dintre de l'element HTML referenciat.
@@ -27,4 +34,70 @@ const app = new Vue({
             this.games.push(game);
         }
     }
+});
+
+/*
+|-------------------------------------------------------------------------------
+| Components de Vue
+|-------------------------------------------------------------------------------
+| Tenint en compte la plantilla HTML generada en la instància:
+| 1. game-header
+| 2. game-add
+| 3. game-list
+|
+*/
+
+/* game-header
+----------------------------------------------------------------------------- */
+Vue.component('game-header', {
+    template: '<h1>Video Games</h1>'
+});
+
+/* game-add
+----------------------------------------------------------------------------- */
+Vue.component('game-add', {
+    // Plantilla
+    template: [
+        '<div>',
+            '<input type="text" v-model="titleGame" />',    // v-model ens permet anar obtenint el valor del input, el qual sera afegit a la variable titleGame.
+            '<button @click="emitNewGame">Añadir</button>', // Dur a terme la funció emitGame quan es faci clic (mètode).
+        '</div>' 
+    ].join(''),
+
+    // L'element data, en un component, s0inicialitza com una funció i no com un objecte (tal i com passa en la instància de Vue).
+    data: function () {
+        return {
+            titleGame: null
+        }
+    },
+
+    // Mètodes
+    methods: {
+        // La funció mira si l'input està buit i, si no ho està, llança ($emit) un esdeveniment al component pare, permetent que sigui traspassat, de pare a fill, el títol introduït en l'input.
+        emitNewGame: function () {
+            if (this.titleGame) {
+                this.$emit('new', { title: this.titleGame });
+                this.titleGame = null;
+            }
+        }
+    },
+});
+
+/* game-list
+El component game-list conté un altre component, game-item.
+----------------------------------------------------------------------------- */
+Vue.component('game-list', {
+    props: ['games'],
+    template: [
+        '<ol>',
+            '<game-item v-for="item in games" :game="item" :key="item.id"></game-item>',
+        '</ol>'
+    ].join('')
+});
+
+Vue.component('game-item', {
+    // Són com els paràmetres d'una funció, en aquest cas variables que són passades del component pare al fill.
+    // Aquestes variables són afegides com si foren atributs (en el pare) => :game="item".
+    props: ['game'],
+    template: '<li>{{ game.title }}</li>'
 });
